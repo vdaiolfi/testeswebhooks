1657aaebcffbf6ffe4b156d401237859dc33e8d8
vdaiolfi <101340088+vdaiolfi@users.noreply.github.com>
2024-11-08 13:49:39 -0300

    Update app.js

--- app.js
+++ app.js
@@ -2,23 +2,6 @@ const fs = require("fs");
 const https = require("https");
 
 const options = {
-    key: fs.readFileSync("server.key"),
-    cert: fs.readFileSync("server.cert")
-};
-
-https 
-    .createServer(options, (req,res) => {
-
-        res.writeHead(200);
-        res.end("Hello Wolrd Using HTTPS!\n");
-    })
-    .listen(3000);
-
-const data = JSON.stringify({
-  "test": "event"
-})
-
-const options = {
   hostname: "https://testeswebhooks.onrender.com",
   port: 443,
   path: "/",
@@ -28,8 +11,3 @@ const options = {
     "Content-Length": data.length,
   },
 }
-
-const req = https.request(options)
-req.write(data)
-req.end()
-
